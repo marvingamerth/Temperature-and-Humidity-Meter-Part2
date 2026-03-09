eb4c960a56b45496004dcb20ad8f286985b81af8
Akarawin Maiphun <111678908+marvingamerth@users.noreply.github.com>
2026-03-09 11:12:19 +0700

    improve buzzer
    Refactor user.ino to improve structure and functionality.

--- user.ino
+++ user.ino
@@ -1,48 +1,38 @@
+#include "config.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include "config.h"
-#include "HX711.h"
-
+//----------don't edit; Default by PCB Design ------------------
 #define SW_PIN 2
-#define LED_GREEN 40
+#define LED_GREEN 42
 #define LED_YELLOW 41
-#define LED_RED 42
+#define LED_RED 40 
+//-------------------------------------------------------
+#define BUZZER 5
+#define DHTPIN 6
+#define DHTTYPE DHT22
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     -1
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-#define TOPIC_USER_DISTANCE TOPIC_PREFIX"/user/distance"
-#define TOPIC_USER_WEIGHT TOPIC_PREFIX"/user/weight"
+
+#define TOPIC_ROOM_TEMP TOPIC_PREFIX"/room/temp"
+#define TOPIC_ROOM_HUMID TOPIC_PREFIX"/room/humid"
 #define TOPIC_NOTI_ON TOPIC_PREFIX"/noti/on"
+#define TOPIC_NOTI_OFF TOPIC_PREFIX"/noti/off"
 
-float calibration_factor =770007.00; 
-#define zero_factor 6870930
-#define DOUT  6
-#define CLK   5
-#define DEC_POINT  2
 
-float offset=0;
-float get_units_kg();
 
-HX711 scale(DOUT, CLK);
 
+//--------------Wifi and MQTT Connecting---------------------------------
 WiFiClient wifiClient;
 PubSubClient mqtt(MQTT_BROKER, 1883, wifiClient);
 uint32_t last_publish;
-uint32_t last_ultrasonic_time =0;
-
-//ultrasonic control------------------------
-const int pingPin = 15;
-const int inPin = 16;
-
-const int MIN_DIST = 2;   // ระยะใกล้สุดที่ยอมรับได้ (cm)
-const int MAX_DIST = 30;  // ระยะไกลสุดที่ต้องการ (cm)
-
-long current_distance = 999;
 
 void connect_wifi() {
   WiFi.mode(WIFI_STA);
@@ -51,176 +41,209 @@ void connect_wifi() {
   printf("Connecting to WiFi %s.\n", WIFI_SSID);
   while (WiFi.status() != WL_CONNECTED) {
     printf(".");
-    digitalWrite(LED_RED, 1);
-    delay(100);
-    digitalWrite(LED_RED, 0);
     fflush(stdout);
+    digitalWrite(LED_RED, 1); //if wifi is not connected, red led is flashing.
+    delay(100);
+    digitalWrite(LED_RED, 0); //----------------
     delay(500);
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Connecting...");
   }
   printf("\nWiFi connected.\n");
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("WiFi Connected!");
+  display.print("IP: ");
+  display.println(WiFi.localIP());
+  display.display();
+  delay(1000);
 }
-
-void mqtt_callback(char* topic, byte* payload, unsigned int length){
-  printf("%s\n", topic);
-}
-
 void connect_mqtt() {
   printf("Connecting to MQTT broker at %s.\n", MQTT_BROKER);
   if (!mqtt.connect("", MQTT_USER, MQTT_PASS)) {
     printf("Failed to connect to MQTT broker.\n");
-    for (;;) {} // wait here forever
+    for (;;){
+      digitalWrite(LED_YELLOW, 1);
+      delay(500);
+      digitalWrite(LED_YELLOW, 0);
+    } // wait here forever
   }
   mqtt.setCallback(mqtt_callback);
   printf("MQTT broker connected.\n");
+  //subscribe for get data from user device and dashboard
+
+  mqtt.subscribe(TOPIC_NOTI_ON);
+  mqtt.subscribe(TOPIC_NOTI_OFF);
 }
 
 
 
-
-
-// --- ย้ายฟังก์ชันมาไว้ข้างบน เพื่อความชัวร์และลด Error ---
-long microsecondsToCentimeters(long microseconds) {
-  // ความเร็วเสียงคือ 29 ไมโครวินาทีต่อเซนติเมตร
-  // หาร 2 เพราะสัญญาณไปและกลับ
-  return microseconds / 29 / 2;
-}
-//-----------------------------------------------------
-
-enum tasksw_state_t{
-  WAIT_SW_PRESS,
-  DEBOUNCE1,
-  WAIT_SW_RELEASE,
-  DEBOUNCE2,
+enum taskled_state_t{
+  LED_OFF,
+  LED_ON,
 };
 
-tasksw_state_t tasksw_state;
+enum taskdht_state_t{
+  DHT_ON,
+  DHT_OFF,
+};
 
-uint32_t timestamp1;
+enum taskbuz_state_t{
+  BUZ_ON,
+  BUZ_OFF,
+};
 
-void tasksw(){
-  uint32_t now;
-  if(tasksw_state == WAIT_SW_PRESS){
-    if(digitalRead(SW_PIN) == 0){
-      tasksw_state = DEBOUNCE1;
-      printf("MQTT publish for stop alert\n");//dummy
-      mqtt.publish(TOPIC_NOTI_ON, "0");
-      timestamp1 = millis();
+taskled_state_t taskled_state;
+taskdht_state_t taskdht_state;
+taskbuz_state_t taskbuz_state;
+uint32_t timestamp1, timestamp2, timestamp3;
+
+bool isNotiActive = false;
+bool yellow_state = false;
+
+void mqtt_callback(char* topic, byte* payload, unsigned int length){
+  printf("%s\n", topic);
+  char message[length+1];
+  memcpy(message, payload, length);
+  message[length] = '\0';
+  if(strcmp(topic, TOPIC_NOTI_ON) == 0){ //temporary alert system
+    if(strcmp(message, "1")==0){
+      isNotiActive = true;
+      digitalWrite(LED_YELLOW, 1);
+    }else if(strcmp(message, "0") == 0){
+      isNotiActive = false;
+      digitalWrite(LED_YELLOW, 0);
+
     }
-  }else if(tasksw_state == DEBOUNCE1){
-    now = millis();
-    if(now - timestamp1 >= 10){
-      tasksw_state = WAIT_SW_RELEASE;
-      timestamp1 = now;
-    }
-  }else if(tasksw_state == WAIT_SW_RELEASE){
-    if(digitalRead(SW_PIN) == 1){
-      tasksw_state = DEBOUNCE2;
-      now = millis();
-    }
-  }else if(tasksw_state == DEBOUNCE2){
-    now = millis();
-    if(now - timestamp1 >= 10){
-      tasksw_state = WAIT_SW_PRESS;
-      timestamp1 = now;
-    }
+    printf("%s\n", message);
   }
+
 }
 
-void taskultrasonic(){
-  if(millis()- last_ultrasonic_time >= 300){
-    last_ultrasonic_time = millis();
 
-    long duration, cm;
-    pinMode(pingPin, OUTPUT);
-    digitalWrite(pingPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(pingPin, HIGH);
-    delayMicroseconds(5);
-    digitalWrite(pingPin, LOW);
-    
-    pinMode(inPin, INPUT);
-
-    duration = pulseIn(inPin, HIGH, 30000);
-
-    if(duration == 0){
-      cm = 999;
-    }else{
-      cm = microsecondsToCentimeters(duration);
-    }
-
-    current_distance = cm;
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0,0);
-    if (cm >= MIN_DIST && cm <= MAX_DIST) {
-      Serial.print("Target Detected: ");
-      Serial.print(cm);
-      Serial.println(" cm");
-      display.print("Dist: ");
-      display.print(cm);
-      display.print(" cm");
-    }else {
-      Serial.println("Out of Range / No Object");
-      display.print("Out of range");
-    }
-    display.display();
-  }
-}
-
-void task_mqtt_publish(){
-  if(millis() - last_publish >= 2000){
-    last_publish = millis();
-
-    if(mqtt.connected()){
-      char payload[20];
-      if(current_distance >= MIN_DIST && current_distance <= MAX_DIST){
-        snprintf(payload, sizeof(payload), "%ld", current_distance);
-      }else{
-        snprintf(payload, sizeof(payload), "out_of_range");
-      }
-
-      mqtt.publish(TOPIC_USER_DISTANCE, payload);
-      printf("Published %s", payload);
-
-      float current_weight = get_units_kg() + offset; // ดึงค่าน้ำหนัก
-      char payload_weight[20];
-      snprintf(payload_weight, sizeof(payload_weight), "%.2f", current_weight); // แปลงทศนิยม 2 ตำแหน่ง
-      
-      mqtt.publish(TOPIC_USER_WEIGHT, payload_weight);
-      printf("Published Weight: %s kg\n", payload_weight);
-
-    }
-  }
-}
+//-------------------set-up--and--loop--------------------------
+DHT dht(DHTPIN, DHTTYPE);
 void setup() {
   Serial.begin(115200);
-  pinMode(SW_PIN, INPUT_PULLUP);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
   pinMode(LED_RED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+
+  digitalWrite(BUZZER, 0); //make buzzer quiet when start.
   Wire.begin(48, 47);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  Serial.println("Load Cell");
-  scale.set_scale(calibration_factor); 
-  scale.set_offset(zero_factor);  
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    printf("SSD1306 allocation failed\n");
+    for(;;); // หยุดถ้าจอเสีย
+  }
+  display.clearDisplay();
+  display.display();
+
+  dht.begin();
   connect_wifi();
   connect_mqtt();
   last_publish = 0;
-}
 
-float get_units_kg()
-{
-  return(scale.get_units()*0.453592);
-}
 
-void loop() {
-  mqtt.loop();
+  taskled_state = LED_ON;
+  taskdht_state = DHT_ON;
 
-  tasksw();
-  taskultrasonic();
-  task_mqtt_publish();
-  Serial.print("Reading: ");
-  String data = String(get_units_kg()+offset, DEC_POINT);
-  Serial.print(data);
-  Serial.println(" kg");
+  timestamp1 = millis();
+  timestamp2 = millis();
+  timestamp3 = millis();
   
+
+  
+}
+
+void taskled() {
+  uint32_t now;
+  if(taskled_state == LED_ON){
+    now = millis();
+    if(now - timestamp1 >= 100){
+      digitalWrite(LED_GREEN, 0);
+      taskled_state = LED_OFF;
+      timestamp1 = now;
+    }
+  }else if(taskled_state == LED_OFF){
+    now = millis();
+    if(now - timestamp1 >= 2900){
+      digitalWrite(LED_GREEN, 1);
+      taskled_state = LED_ON;
+      timestamp1 = now;
+    }
+  }
+}
+
+void taskbuz() { 
+  if(isNotiActive){
+    uint32_t now = millis();
+    if(taskbuz_state == BUZ_ON){
+      if(now - timestamp3 >= 300){
+        digitalWrite(BUZZER, 0);
+        //digitalWrite(LED_RED, 0); // สั่ง 1 ให้เงียบ
+        taskbuz_state = BUZ_OFF;
+        timestamp3 = now;
+      }
+    }else if(taskbuz_state == BUZ_OFF){
+      if(now - timestamp3 >= 100){
+        digitalWrite(BUZZER, 1);
+        //digitalWrite(LED_RED, 1); // สั่ง 0 ให้ดัง
+        taskbuz_state = BUZ_ON;
+        timestamp3 = now;
+      }
+    }
+  } else {
+    // ป้องกันกรณีระบบค้าง ให้บัซเซอร์เงียบเสมอถ้าไม่ได้อยู่ในโหมดแจ้งเตือน
+    digitalWrite(BUZZER, 0);
+    digitalWrite(LED_RED, 0); 
+  }
+}
+
+
+void taskdht(){
+  uint32_t now = millis();
+  if(now - timestamp2 >= 5000){
+    timestamp2 = now;
+    float t = dht.readTemperature();
+    float h = dht.readHumidity();
+    if(!isnan(t) && !isnan(h)){
+      String temp = String(t, 2);
+      mqtt.publish(TOPIC_ROOM_TEMP, temp.c_str());
+      printf("Publishing TEMP Value (C): %.2f\n", t);
+
+      String humid = String(h, 2);
+      mqtt.publish(TOPIC_ROOM_HUMID, humid.c_str());
+      printf("Publishing HUMIDITY Value: %.2f\n", h);
+
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setTextColor(SSD1306_WHITE);
+      display.setCursor(0,0);
+      display.print("TEMP(C):");
+      display.setCursor(93, 0);
+      display.print(temp);
+      display.setCursor(0, 35);
+      display.print("HUMID(%):");
+      display.setCursor(93, 35);
+      display.print(humid);
+      display.display();
+    }
+    taskdht_state = DHT_OFF;
+    timestamp2 = now;
+  }
+  
+  
+}
+
+void loop(){
+  
+  mqtt.loop();
+  taskled();
+  taskdht();
+  taskbuz();
+
+  
+
 }
